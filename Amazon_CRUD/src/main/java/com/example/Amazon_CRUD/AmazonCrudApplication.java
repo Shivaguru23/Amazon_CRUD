650cdb4e8c67941889f991a01fbb9b816195e10d
Shivaguru23 <shivaguru7687@gmail.com>
2025-09-06 06:31:57 +0530

    Adding Local files 1

---
+++ Amazon_CRUD/src/main/java/com/example/Amazon_CRUD/AmazonCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.Amazon_CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonCrudApplication.class, args);
+	}
+
+}
---
+++ Amazon_CRUD/src/test/java/com/example/Amazon_CRUD/AmazonCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Amazon_CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
